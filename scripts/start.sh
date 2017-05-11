d3a37a9b049a8975aef77a2d12fc70b6ec5eee14
Ash Williams <housewares@users.noreply.github.com>
2017-05-11 17:03:18 +0300

    Use internal env. vars.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PHP_FPM_CONF='/etc/php5/php-fpm.conf'
-PHP_INI='/etc/php5/php.ini'
-
 # Disable Strict Host checking for non interactive git clones
 
 mkdir -p -m 0700 /root/.ssh
@@ -79,16 +76,16 @@ fi
 
 # Display PHP error's or not
 if [[ "$ERRORS" != "1" ]] ; then
- echo php_flag[display_errors] = off >> "${PHP_FPM_CONF}"
+ echo php_flag[display_errors] = off >> "${fpm_conf}"
 else
- echo php_flag[display_errors] = on >> "${PHP_FPM_CONF}"
+ echo php_flag[display_errors] = on >> "${fpm_conf}"
 fi
 
 # Display Version Details or not
 if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
  sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
 else
- sed -i "s/expose_php = On/expose_php = Off/g" "${PHP_FPM_CONF}"
+ sed -i "s/expose_php = On/expose_php = Off/g" "${fpm_conf}"
 fi
 
 # Pass real-ip to logs when behind ELB, etc
@@ -112,17 +109,17 @@ fi
 
 # Increase the memory_limit
 if [ ! -z "$PHP_MEM_LIMIT" ]; then
- sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" "${PHP_INI}"
+ sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" "${php_conf}"
 fi
 
 # Increase the post_max_size
 if [ ! -z "$PHP_POST_MAX_SIZE" ]; then
- sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" "${PHP_INI}"
+ sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" "${php_conf}"
 fi
 
 # Increase the upload_max_filesize
 if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
- sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" "${PHP_INI}"
+ sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" "${php_conf}"
 fi
 
 if [ ! -z "$PUID" ]; then
